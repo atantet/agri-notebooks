a4ccc69c1e2fa26709eea75cd92a4cac43b9f01a
Tantet Alexis (M.) <alexis.tantet@lmd.ipsl.fr>
2025-01-13 13:31:26 +0100

    remove pyinstaller

--- launcher.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# Path to the notebook and port
-notebook = "bilan_hydrique_observation_horaire.ipynb"
-port = "5006"
-
-# Start the Panel server
-subprocess.run(["panel", "serve", notebook, "--port", port])
